89b1c965bc2e77943d1106ca02fc107072cf0816
Garvit Pahal <g10pahal@gmail.com>
2022-02-24 17:57:56 +0530

    Refactor crowdfunding code

--- quests/31-crowdfunding/src/lib.rs
+++ quests/31-crowdfunding/src/lib.rs
@@ -30,34 +30,32 @@ pub fn process_instruction(
 
     let description = String::from_utf8(rest_of_data[9..].to_vec()).unwrap();
 
-    if *instruction_byte == 0 {
-        let campaign_owner_account = next_account_info(accounts_iter)?;
-        let mut campaign_account_data =
-            CampaignAccount::try_from_slice(&campaign_account.data.borrow())?;
-        campaign_account_data.campaign_owner = *campaign_owner_account.owner;
-        campaign_account_data.campaign_amount = amount;
-        campaign_account_data.campaign_description = description;
-        campaign_account_data.campaign_fulfilled = 0;
-        campaign_account_data.serialize(&mut &mut campaign_account.data.borrow_mut()[..])?;
+    match *instruction_byte {
+        0 => {
+            let campaign_owner_account = next_account_info(accounts_iter)?;
+            let mut campaign_account_data =
+                CampaignAccount::try_from_slice(&campaign_account.data.borrow())?;
+            campaign_account_data.campaign_owner = *campaign_owner_account.owner;
+            campaign_account_data.campaign_amount = amount;
+            campaign_account_data.campaign_description = description;
+            campaign_account_data.campaign_fulfilled = 0;
+            campaign_account_data.serialize(&mut &mut campaign_account.data.borrow_mut()[..])?;
+        }
+        1 => {
+            let campaign_account_data =
+                CampaignAccount::try_from_slice(&campaign_account.data.borrow())?;
+            msg!(
+                "{}",
+                campaign_account_data.campaign_amount - campaign_account_data.campaign_fulfilled,
+            );
+        }
+        _ => {}
     }
-
-    if *instruction_byte == 1 {
-        let campaign_account_data =
-            CampaignAccount::try_from_slice(&campaign_account.data.borrow())?;
-        msg!(
-            "{}",
-            campaign_account_data.campaign_amount - campaign_account_data.campaign_fulfilled
-        );
-    }
-
-    if *instruction_byte == 2 {}
-
-    if *instruction_byte == 3 {}
 
     Ok(())
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
 pub struct CampaignAccount {
     pub campaign_owner: Pubkey,
     pub campaign_amount: u64,
